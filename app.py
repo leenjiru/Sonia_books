f94375b4c231ff626182336a0e83999845906d31
Nick Lee <31591942+LEENICK76@users.noreply.github.com>
2020-05-22 17:36:33 +0300

    add sqlalchemy fuctionality to all files

--- app.py
+++ app.py
@@ -7,15 +7,19 @@ from resources.books import Bookslist, Book
 from resources.users import RegisterUsers
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////mnt/C:/Users/LEE/PycharmProjects/Sonia_Restful/data.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'secret'
 api = Api(app)
 
+
 jwt = JWT(app, authenticate, identity)  # authentication
-books = []
 
 api.add_resource(Book, '/book/<string:name>')
 api.add_resource(Bookslist, '/books')
 api.add_resource(RegisterUsers, '/register')
 
 if __name__ == '__main__':
+    from db import db
+    db.init_app(app)
     app.run(debug=True)
