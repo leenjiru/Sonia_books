402112811e9bd28c2e0f2d915d9befc0b383639c
Chege Bryan <cbbryan8@gmail.com>
2020-05-29 22:17:36 +0300

    ♻ Refactor code

--- resources/users.py
+++ resources/users.py
@@ -1,3 +1,5 @@
+import sqlite3
+
 from flask_restful import Resource, reqparse
 from models.users import UserModel
 
@@ -22,7 +24,15 @@ class RegisterUsers(Resource):
         if UserModel.find_by_username(data['username']):
             return {"message": "username already taken"}, 400
 
-        user = UserModel(**data)
-        user.save_to_db()
+        connection = sqlite3.connect('data.db')
+
+        cursor = connection.cursor()
+
+        query = "INSERT INTO users VALUES (NULL, ?, ?)"
+        cursor.execute(query, (data['username'], data['password']))
+
+        connection.commit()
+
+        connection.close()
 
         return {"message": "User registered successfully"}, 201  # created
